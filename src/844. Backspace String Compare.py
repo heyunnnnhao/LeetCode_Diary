225b5723cc213f16b8ae93e664f73017ba913f7e
heyunnnnhao <48147981+heyunnnnhao@users.noreply.github.com>
2019-11-06 19:57:47 -0500

    Add files via upload

---
+++ src/844. Backspace String Compare.py
@@ -0,0 +1,27 @@
+# solution 1
+def backspaceCompare(self, S, T):
+    def F(S):
+        skip = 0
+        for x in reversed(S):
+            if x == "#":
+                skip += 1
+            elif skip:
+                skip -= 1
+            else:
+                yield x
+
+    return all(x == y for x, y in itertools.izip_longest(F(S), F(T)))
+
+
+# solution 2
+def backspaceCompare(self, S, T):
+    def build(S):
+        ans = []
+        for c in S:
+            if c != "#":
+                ans.append(c)
+            elif ans:
+                ans.pop()
+        return "".join(ans)
+
+    return build(S) == build(T)
